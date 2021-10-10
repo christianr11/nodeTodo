3ad4c0be7d3a8c0726f8bbae7726b32a7f53c9d0
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2021-10-10 22:54:32 +0800

    added corse

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const connectDb = require("./database/connection");
 const todoRoutes = require("./routes/todoRoutes");
+const cors = require("cors");
 
 const app = express();
 
 connectDb();
 
 app.use(express.json());
+app.use(cors());
 app.use("/api/v1/todo", todoRoutes);
 
 app.use(express.json());
